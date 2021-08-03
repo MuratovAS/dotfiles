eaaec842e904f6d9f5ecdc69261936210f8fe4f2
MuratovAS <lexamzx@gmail.com>
2021-08-03 18:33:46 +0500

    Optimization, using signals

--- .config/sway/scripts/sunset.sh
+++ .config/sway/scripts/sunset.sh
@@ -1,52 +1,38 @@
-#!/bin/sh
-#Checks if a configuration file exists, if necessary creates 
-function checkConfig(){
-	if [ ! -f "$HOME/.config/wlsunset/config" ];
-	then 
-		mkdir -p $HOME/.config/wlsunset
-echo 'temp_low="4000"
-temp_high="6500"
-sunrise="07:00"
-sunset="23:00"
-duration="900"
-location="off"
-' >> $HOME/.config/wlsunset/config
-	fi
-}
+#!/bin/bash
 
 #Startup function
-function onSunSet(){
-	checkConfig
-	source $HOME/.config/wlsunset/config
+function start(){
+	[[ -f "$HOME/.config/wlsunset/config" ]] && source "$HOME/.config/wlsunset/config"
+	temp_low=${temp_low:-"4000"}
+	temp_high=${temp_high:-"6500"}
+	duration=${duration:-"900"}
+	sunrise=${sunrise:-"07:00"}
+	sunset=${sunset:-"23:00"}
+	longitude=${longitude:-65}
+	latitude=${latitude:-65}
+	location=${location:-"off"}
 
-	if [ ${location} = "on" ]; 
+	if [ "${location}" = "on" ]; 
 	then
 		CONTENT=$(curl -s https://freegeoip.app/json/)
-		longitude=$(echo $CONTENT | jq .longitude)
-		latitude=$(echo $CONTENT | jq .latitude)
-		if [ -e $longitude ];
-		then
-			echo location ERROR: freegeoip.app
-			longitude='65'
-			latitude='60'
-		else
-			echo location OK:  $latitude $longitude
-		fi	
+		content_longitude=$(echo $CONTENT | jq '.longitude // empty')
+        longitude=${content_longitude:-"${longitude}"}
+        content_latitude=$(echo $CONTENT | jq '.latitude // empty')
+        latitude=${content_latitude:-"${latitude}"}
 		wlsunset -l $latitude -L $longitude -t $temp_low -T $temp_high -d $duration &
 	else
-		wlsunset -t $temp_low -T $temp_high  -d $duration -S $sunrise -s $sunset &
+		wlsunset -t $temp_low -T $temp_high -d $duration -S $sunrise -s $sunset &
 	fi
-
 }
 
 #Accepts managing parameter 
 case $1'' in
 	'off')
    	pkill wlsunset
-    ;;
+	;;
 
 	'on')
-	onSunSet
+	start
 	;;
 
 	'toggle')
@@ -54,7 +40,21 @@ case $1'' in
 	then
 		pkill wlsunset
 	else
-		onSunSet
+		start
 	fi
     ;;
+    'check')
+    command -v wlsunset
+    exit $?
+    ;;
 esac
+
+#Returns a string for Waybar 
+if pkill -0 wlsunset
+then
+	class="on"
+else
+	class="off"
+fi	
+
+printf '{"alt":"%s"}\n' "$class"
